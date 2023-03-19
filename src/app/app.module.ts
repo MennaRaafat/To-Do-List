102cd6810d0620d41fa3699c2584da3d11d98d36
MennaRaafat <menna.raafat.hamed@commerce.helwan.edu.eg>
2023-03-19 22:10:29 +0200

    to-do app completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
@@ -14,7 +13,7 @@ import { TodosComponent } from './todos/todos.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
